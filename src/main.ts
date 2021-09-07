75a282a70ce84abf7f87adcdc884ee6f73f1ff09
Brian Pina <brian.pina2001@gmail.com>
2021-09-06 21:05:21 -0300

    setup project

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as helmet from 'helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('Studyt')
+    .setDescription('API para a aplicação Studyt')
+    .setVersion('0.0')
+    .addTag('studyt')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.enableShutdownHooks();
+
+  await app.listen(3000);
+}
+bootstrap();
