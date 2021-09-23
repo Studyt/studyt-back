895ef9e451ec1a61aba752ed0161d09152a953a8
Victor-Buendia <victorbuendia03@gmail.com>
2021-09-23 12:25:11 -0300

    Fix auth status codes
    Co-authored-by: Arthur Matos <git.arthurmatos@gmail.com>
    Co-authored-by: Brian Pina de Sá <brian.pina2001@gmail.com>

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post, UseGuards } from '@nestjs/common';
 import { LoginDTO } from 'src/common/dtos/login.dto';
 import { RegisterDTO } from '../common/dtos/register.dto';
 import { AuthService } from './auth.service';
@@ -6,9 +6,10 @@ import { JwtGuard } from './jwt/jwt.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
   @Post('login')
+  @HttpCode(200)
   async login(@Body() loginDTO: LoginDTO) {
     const { email, password } = loginDTO;
     return this.authService.login(email, password);
@@ -20,6 +21,7 @@ export class AuthController {
   }
 
   @Post('confirmation/:token')
+  @HttpCode(200)
   async confirmation(@Param('token') token: string) {
     return this.authService.confirmToken(token);
   }
